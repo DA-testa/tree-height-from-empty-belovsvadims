735104ead4fc37c4375b1ea09124526ee156b251
Vadim <58696690+belovsvadims@users.noreply.github.com>
2023-03-06 01:24:37 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -42,8 +42,6 @@ def main():
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
     
-    nodes = None
-    parents1 = None
     choice = input("I or F: ")
     if choice == "I":
         nodes = int(input("Enter number of nodes: "))
