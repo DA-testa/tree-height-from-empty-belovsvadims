9c7abe903ba80ef8d265a6c9c2e33e6985e9c874
Vadim <58696690+belovsvadims@users.noreply.github.com>
2023-03-05 21:31:55 +0000

    221RDB129 Vadims Belovs 11.grupa

--- tree_height.py
+++ tree_height.py
@@ -1,15 +1,35 @@
 # python3
+# 221RDB129 Vadims Belovs 11.grupa
 
 import sys
 import threading
 import numpy
 
+def calculate(parents, i, height):
+    if height[i] != 0:
+        return
+    
+    if parents[i] == -1:
+        height[i] = 1
+        return
+    
+    if height[parents[i]] == 0:
+        calculate(parents, parents[i], height)
+
+    height[i] = height[parents[i]] + 1
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
+    height = [0] * n
+
+    for i in range(n):
+        calculate(parents, i, height)
+
+    max_height = height[0]
+    for i in range(1, n):
+        max_height = max(max_height, height[i])
+
     return max_height
+
 
 
 def main():
@@ -17,11 +37,30 @@ def main():
     
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
-    
+
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
+
+    choice = input("I or F: ")
+    if choice == "I":
+        n = int(input("Number of nodes: "))
+        parents = list(map(int, input("Parents: ").split()))
+        print(compute_height(n, parents))
+        
+
+    elif choice == "F":
+        fPath = input("File path: ")
+        if 'a' not in fPath:
+            with open(str(fPath)) as f:
+                n = int(f.readline())
+                parents = list(map(int, f.readline().split()))
+                print(compute_height(n, parents))
+        else:
+            print("Using letter 'a' in filepath is forbidden")
+    else:
+        print("Wrong letter! Choose I or F")
+    
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
