c52a0fbd6d929ff54afb7b8901cc08e5f36d26fb
Vadim <58696690+belovsvadims@users.noreply.github.com>
2023-03-06 03:18:27 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,28 +6,28 @@ import threading
 import numpy
 
 def compute_height(n, parents):
-    height = numpy.zeros(n)
+    height = [0] * n
     def calculate(i):
         if height[i] != 0:
-            return height[i]
+            return
+    
         if parents[i] == -1:
             height[i] = 1
-        else:
-            height[i] = calculate([parents[i]]) + 1
-        return height[i]
+            return
+    
+        if height[parents[i]] == 0:
+            calculate(parents[i])
+
+        height[i] = height[parents[i]] + 1
 
     for i in range(n):
         calculate(i)
-    return int(max(height))
 
-    """
     max_height = height[0]
     for i in range(1, n):
         max_height = max(max_height, height[i])
 
     return max_height
-    """
-
 
 def main():
     # implement input form keyboard and from files
@@ -38,24 +38,21 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    
+
     choice = input()
     if choice == "I":
         n = int(input())
         parents = list(map(int, input().split()))
-        print(compute_height(n, parents))
+        
+
     elif choice == "F":
         fPath = input()
         if 'a' not in fPath:
-            with open(str("test/"+fPath), "r") as f:
+            with open(str("test/"+fPath)) as f:
                 n = int(f.readline())
                 parents = list(map(int, f.readline().split()))
-                print(compute_height(n, parents))
-        else:
-          print("Error")
-    else:
-      print("Error")
-    
+                    
+    print(compute_height(n, parents))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
